12ff74494bb50a51de2112e19e9f40f273486cc5
Orca Monsta <OrcaMonsta@gmail.com>
2024-11-11 17:19:52 -0400

    Update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
+      fontFamily: {
+        cornerstone: ['var(--font-cornerstone)'],
+      },
       animation: {
         'grid-flow': 'grid-flow 20s linear infinite',
+        'slow-spin': 'slow-spin 30s linear infinite',
+        '3d-spin': 'rotate3d 20s linear infinite',
       },
       keyframes: {
         'grid-flow': {
           '0%': { transform: 'translateY(0)' },
           '100%': { transform: 'translateY(-100%)' },
         },
-=======
-      fontFamily: {
-        cornerstone: ['var(--font-cornerstone)'],
-      },
-      animation: {
-        'slow-spin': 'slow-spin 30s linear infinite',
-        '3d-spin': 'rotate3d 20s linear infinite',
->>>>>>> 4ce5a324ecba300cddb21860b7577723c390dafb
       },
     },
   },
