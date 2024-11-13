ffa683812527f2379eb06d480fa5f57d804106e8
Orca Monsta <OrcaMonsta@gmail.com>
2024-11-13 10:25:00 -0400

    Add website launch and Gobbler AMM implementation announcements

--- app/announcements/page.tsx
+++ app/announcements/page.tsx
@@ -32,6 +32,41 @@ const announcements = [
     content: "Introducing our new community rewards program with continous benefits for holders. Fees generated from daily trading volume will be distributed to holders proportionally.",
     category: "COMMUNITY",
     important: false
+  },
+  {
+    date: "2024-11-02",
+    title: "Community spaces launched",
+    content: "Telegram channels launched for community interaction and updates, Twitter account has be launched and will be used for announcements and updates, Discord server launched for more community interaction and to host our community events.",
+    category: "Launch",
+    important: true
+  },
+  {
+    date: "2024-11-03",
+    title: "WEBSITE IS LIVE",
+    content: "We are excited to announce our website has been launched and is live, construction is still being conducted so stay tuned for updates as we implement them! When fully completed we will be migrating our server to an HTTPS domain.",
+    category: "DEVELOPMENT",
+    important: true
+  },
+  {
+    date: "2024-11-06",
+    title: "GOBBLER AMM IMPLEMENTATION",
+    content: "Gobbler is an AMM on Solana that uses a flat fee structure instead of % meaning that it should default on aggregators as the cheapest trade in most cases providing there's liquidity. We also have the LP tokens having metadata so modify them and the LP tokens themselves are on bonding curves accordingly. Gobbler will be embedded on our platform and will be available for use.",
+    category: "DEVELOPMENT",
+    important: true
+  },
+  {
+    date: "2024-11-12",
+    title: "Twitter buy and burn implementation",
+    content: "Our developers have implemented a buy and burn mechanism on Twitter. linking our platform to our Twitter mechanism, users will be able to trade live on X.com by posting and commenting. When a user buys using our automated platform, a portion of the transaction will be used to buy $ORCA and burn it. This will help drive up the price of $ORCA and any other ecosystem tokens traded on X (Twitter) as there will inheirently be less supply.",
+    category: "DEVELOPMENT",
+    important: true
+  },
+  {
+    date: "2024-11-13",
+    title: "NFT Development",
+    content: "NFTs are being developed for the community, these will be used general trading, to enchance our ecosystem and for giveaways and future community events. More information will be released soon as it becomes available.",
+    category: "DEVELOPMENT",
+    important: true
   }
 ]
 
@@ -80,7 +115,7 @@ export default function Announcements() {
         
         {/* Add current date display */}
         <div className={`${cornerstone.className} text-xl text-gray-400 mb-8`}>
-          CURRENT DATE: {getCurrentDate()}
+          TODAY'S DATE: {getCurrentDate()}
         </div>
 
         {/* Announcements List */}
